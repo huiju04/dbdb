aa127d431d422c1aa2503dee3bcf04a6d77c96cb
ayg08 <ayg@oneplat.co>
2023-11-23 22:07:00 +0900

    .

---
+++ src/page/Profile.js
@@ -0,0 +1,10 @@
+const profileData = {
+    bangsung: {
+        name: '백설공주',
+        description: '백설공주는 제 친구입니다.
+    },
+    gildong: {
+        name: '백설홍길',
+        description: '동화속은 주인공입니다'
+    }
+}
